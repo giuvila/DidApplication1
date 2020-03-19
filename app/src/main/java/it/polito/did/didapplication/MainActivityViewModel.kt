613c177233489aca1b0b38345a953faa6b0b68bb
Simo <simonesegalini10@gmail.com>
2020-03-19 12:09:42 +0100

    fragmentHome

--- app/src/main/java/it/polito/did/didapplication/MainActivityViewModel.kt
+++ app/src/main/java/it/polito/did/didapplication/MainActivityViewModel.kt
@@ -2,10 +2,15 @@ package it.polito.did.didapplication
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import org.json.JSONObject
 
 class ViewModel : ViewModel() {
-    var email = "";
-    var password = "";
+    var email = ""
+    var password = ""
+    var name = "Mario"
+    var surname = "Rossi"
+    var occupation = "Designer"
+
     init {
         Log.i("VIEWMODEL", "ViewModel created!")
     }
